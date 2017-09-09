9903e722bbfe11dab4ebebcfc170a9f7b6102951
adityawebsite <websiteaditya@gmail.com>
2017-09-09 21:06:06 +0700

    Update playgame.php

--- includes/playgame.php
+++ includes/playgame.php
@@ -3,13 +3,13 @@ $nama = $_SESSION["masuk"];
 $spemain = mysqli_query($connection, "select * from pemain where nama='$nama'");
 $gpemain = mysqli_fetch_array($spemain);
 $pesan="";
-$connection = mysqli_connect("localhost", "root", "");
+$connection = mysqli_connect("173.208.189.34", "adityaw1_wp197", "Putuaditya97");
 $db = mysqli_select_db($connection, "adityaw1_wp197");
 $groom = mysqli_fetch_array(mysqli_query($connection, "select * from room where id='$id'"));
 $pemain = mysqli_query($connection, "select * from pemain");
 
 function menang() {
-	$connection = mysqli_connect("localhost", "root", "");
+	$connection = mysqli_connect("173.208.189.34", "adityaw1_wp197", "Putuaditya97");
 	$db = mysqli_select_db($connection, "adityaw1_wp197");
 	$idrooms = $_GET["id"];
 	$groom = mysqli_fetch_array(mysqli_query($connection, "select * from room where id='$idrooms'"));
@@ -34,20 +34,20 @@ function menang() {
 function login(){
 	if(isset($_SESSION["login"])) { 
 		$id = $_GET["id"];
-		$connection = mysqli_connect("localhost", "root", "");
+		$connection = mysqli_connect("173.208.189.34", "adityaw1_wp197", "Putuaditya97");
 		$db = mysqli_select_db($connection, "adityaw1_wp197");
 		$groom = mysqli_fetch_array(mysqli_query($connection, "select * from room where id='$id'"));
 		?>
 			<table>
 			  <tr>
-		 	  <td style="text-align:left"><a class="btn btn-primary" href="/ww/play.php?action=adminmalam&id=<?php echo $id; ?>">1. Malam</a></td>
-		 	  <td style="text-align:center"><a class="btn btn-primary" href="/ww/play.php?action=admindebat&id=<?php echo $id; ?>">2. Siang</a></td>
-		  	  <td style="text-align:right"><a class="btn btn-primary" href="/ww/play.php?action=adminvote&id=<?php echo $id; ?>">3. Vote</a></td>
+		 	  <td style="text-align:left"><a class="btn btn-primary" href="/play.php?action=adminmalam&id=<?php echo $id; ?>">1. Malam</a></td>
+		 	  <td style="text-align:center"><a class="btn btn-primary" href="/play.php?action=admindebat&id=<?php echo $id; ?>">2. Siang</a></td>
+		  	  <td style="text-align:right"><a class="btn btn-primary" href="/play.php?action=adminvote&id=<?php echo $id; ?>">3. Vote</a></td>
 			  <?php if( $groom["hasilvote"] == 1) { ?>
-			  <td style="text-align:right"><a class="btn btn-primary" href="/ww/play.php?action=vote2&id=<?php echo $id; ?>">3. Vote Lagi</a></td>
+			  <td style="text-align:right"><a class="btn btn-primary" href="/play.php?action=vote2&id=<?php echo $id; ?>">3. Vote Lagi</a></td>
 			  <?php } ?>
 			  <?php if($groom["vote"] == 1 || $groom["vote2"] == 1) { ?>
-			  <td style="text-align:right"><a class="btn btn-primary" href="/ww/play.php?action=hasilvote&id=<?php echo $id; ?>">4. Hasil Vote</a></td>
+			  <td style="text-align:right"><a class="btn btn-primary" href="/play.php?action=hasilvote&id=<?php echo $id; ?>">4. Hasil Vote</a></td>
 			  <?php } ?>
 		 	  </tr>
 		    </table>
@@ -56,11 +56,11 @@ function login(){
 }
 function refresh(){
 $id = $_GET["id"];
-$connection = mysqli_connect("localhost", "root", "");
+$connection = mysqli_connect("173.208.189.34", "adityaw1_wp197", "Putuaditya97");
 $db = mysqli_select_db($connection, "adityaw1_wp197");
 $groom = mysqli_fetch_array(mysqli_query($connection, "select * from room where id='$id'"));
 if($groom["selesai"] == 0 ) {
-?>  <form action ="/ww/play.php?id=<?php echo $id; ?>" method="POST">
+?>  <form action ="/play.php?id=<?php echo $id; ?>" method="POST">
 	<button class="btn btn-primary" name="yakin" type="submit">Refresh</button>
 	</form> <?php
 	$nama = $_SESSION["masuk"];
@@ -69,94 +69,94 @@ if($groom["selesai"] == 0 ) {
 	$pesan = mysqli_fetch_array(mysqli_query($connection, "select * from pesan where id='1'"));
 	if(isset($_POST["yakin"])){
 		if($groom["vote"] == "0" && $groom["malam"] == "0" && $groom["debat"] == "0" && $groom["hasilvote"] == "0" && $groom["vote2"] == "0" ) {
-			return Main::redirect(array("danger",e("Menunggu room penuh.")),"/ww/play.php?id=".$groom['id']."");
+			return Main::redirect(array("danger",e("Menunggu room penuh.")),"/play.php?id=".$groom['id']."");
 		}
 		if($groom["vote"] == 1) {
 			if($gpemain["hidup"] == 0){
-				return Main::redirect(array("danger",e("Anda sudah mati. (voting telah dibuka)")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Anda sudah mati. (voting telah dibuka)")),"/play.php?id=".$groom['id']."");
 			}
 			if($gpemain["milih"] == 1) {
-					return Main::redirect(array("success",e("Anda sudah mem-vote. Tunggu hingga hasil vote ditampilkan.")),"/ww/play.php?id=".$groom['id']."");
+					return Main::redirect(array("success",e("Anda sudah mem-vote. Tunggu hingga hasil vote ditampilkan.")),"/play.php?id=".$groom['id']."");
 			}
-			return Main::redirect(array("danger",e("Voting telah dibuka, silahkan memilih pemain yang akan dibunuh.")),"/ww/play.php?id=".$groom['id']."");
+			return Main::redirect(array("danger",e("Voting telah dibuka, silahkan memilih pemain yang akan dibunuh.")),"/play.php?id=".$groom['id']."");
 		}
 		if($groom["malam"] == 1) {
 			if($gpemain["hidup"] == 0){
-				return Main::redirect(array("danger",e("Anda sudah mati. (Hari sudah malam)")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Anda sudah mati. (Hari sudah malam)")),"/play.php?id=".$groom['id']."");
 			}
 			if($_SESSION["karakter"] == "Werewolf"){
 				if($gpemain["bunuh"] == 0) {
-					return Main::redirect(array("danger",e("Malam telah tiba, bunuhlah salah satu pemain.")),"/ww/play.php?id=".$groom['id']."");
+					return Main::redirect(array("danger",e("Malam telah tiba, bunuhlah salah satu pemain.")),"/play.php?id=".$groom['id']."");
 				}
-				return Main::redirect(array("success",e("Tunggu pagi hari tiba.")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("success",e("Tunggu pagi hari tiba.")),"/play.php?id=".$groom['id']."");
 			}
 			if($_SESSION["karakter"] == "Seer"){
 				if($gpemain["terawang"] == 0) {
-					return Main::redirect(array("danger",e("Malam telah tiba, terawanglah salah satu pemain yang belum pernah diterawang.")),"/ww/play.php?id=".$groom['id']."");
+					return Main::redirect(array("danger",e("Malam telah tiba, terawanglah salah satu pemain yang belum pernah diterawang.")),"/play.php?id=".$groom['id']."");
 				}
-				return Main::redirect(array("success",e("Tunggu pagi hari tiba.")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("success",e("Tunggu pagi hari tiba.")),"/play.php?id=".$groom['id']."");
 			}
 			if($_SESSION["karakter"] == "Guard"){
 				if($gpemain["lindungi"] == 0) {
-					return Main::redirect(array("danger",e("Malam telah tiba, lindungilah salah satu pemain dari ancaman pembunuhan werewolf.")),"/ww/play.php?id=".$groom['id']."");
+					return Main::redirect(array("danger",e("Malam telah tiba, lindungilah salah satu pemain dari ancaman pembunuhan werewolf.")),"/play.php?id=".$groom['id']."");
 				}
-				return Main::redirect(array("danger",e("Tunggu pagi hari tiba.")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Tunggu pagi hari tiba.")),"/play.php?id=".$groom['id']."");
 			}
 			if($_SESSION["karakter"] == "Villager"){
-				return Main::redirect(array("danger",e("Tidur coeg, ini malam hari.")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Tidur coeg, ini malam hari.")),"/play.php?id=".$groom['id']."");
 			}
 		}
 		if($groom["debat"] == 1) {
 			if($gpemain["hidup"] == 0){
-				return Main::redirect(array("danger",e("Anda sudah mati. Info: ".$pesan['pesan']."")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Anda sudah mati. Info: ".$pesan['pesan']."")),"/play.php?id=".$groom['id']."");
 			}
-			return Main::redirect(array("success",e("".$pesan['debat']."")),"/ww/play.php?id=".$groom['id']."");
+			return Main::redirect(array("success",e("".$pesan['debat']."")),"/play.php?id=".$groom['id']."");
 		}
 		if($groom["hasilvote"] == 1) {
 			if($gpemain["hidup"] == 0){
-				return Main::redirect(array("danger",e("Anda sudah mati. Info: ".$pesan['pesan']."")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Anda sudah mati. Info: ".$pesan['pesan']."")),"/play.php?id=".$groom['id']."");
 			}
-			return Main::redirect(array("success",e("".$pesan['vote']."")),"/ww/play.php?id=".$groom['id']."");
+			return Main::redirect(array("success",e("".$pesan['vote']."")),"/play.php?id=".$groom['id']."");
 		}
 		if($groom["vote2"] == 1) {
 			if($gpemain["hidup"] == 0){
-				return Main::redirect(array("danger",e("Anda sudah mati. (voting telah dibuka)")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Anda sudah mati. (voting telah dibuka)")),"/play.php?id=".$groom['id']."");
 			}
 			if($gpemain["milih"] == 1) {
-					return Main::redirect(array("success",e("Anda sudah mem-vote. Tunggu hingga hasil vote ditampilkan.")),"/ww/play.php?id=".$groom['id']."");
+					return Main::redirect(array("success",e("Anda sudah mem-vote. Tunggu hingga hasil vote ditampilkan.")),"/play.php?id=".$groom['id']."");
 			}
 			if($gpemain["milih2"] == 0){
-				return Main::redirect(array("danger",e("Voting kedua telah dibuka, tetapi anda tidak bisa memilih.")),"/ww/play.php?id=".$groom['id']."");
+				return Main::redirect(array("danger",e("Voting kedua telah dibuka, tetapi anda tidak bisa memilih.")),"/play.php?id=".$groom['id']."");
 			}
-			return Main::redirect(array("danger",e("Voting telah dibuka, silahkan memilih pemain yang akan dibunuh.")),"/ww/play.php?id=".$groom['id']."");
+			return Main::redirect(array("danger",e("Voting telah dibuka, silahkan memilih pemain yang akan dibunuh.")),"/play.php?id=".$groom['id']."");
 		}
 	}
 }
 }
 function cek(){
 $nama = $_SESSION["masuk"];
-$connection = mysqli_connect("localhost", "root", "");
+$connection = mysqli_connect("173.208.189.34", "adityaw1_wp197", "Putuaditya97");
 $db = mysqli_select_db($connection, "adityaw1_wp197");
 $id = $_GET["id"];
 $gpemain = mysqli_fetch_array(mysqli_query($connection, "select * from pemain where nama='$nama'"));
 $groom = mysqli_fetch_array(mysqli_query($connection, "select * from room where id='$id'"));
 	if($groom["vote"] == 1 && $gpemain["hidup"] == 1 || $gpemain["milih2"] == 1) { 
-		 ?><a class="btn btn-primary pull-right" href="/ww/play.php?action=vote&id=<?php echo $id; ?>">Vote</a> <?php		  
+		 ?><a class="btn btn-primary pull-right" href="/play.php?action=vote&id=<?php echo $id; ?>">Vote</a> <?php		  
 	}
 	if($groom["malam"] == 1 && $gpemain["hidup"] == 1) {
 		if($_SESSION["karakter"] == "Werewolf"){
 			if(!isset($_SESSION["bunuh"])) {
-				?><a class="btn btn-primary pull-right" href="/ww/play.php?action=bunuh&id=<?php echo $id; ?>">Bunuh</a> <?php	
+				?><a class="btn btn-primary pull-right" href="/play.php?action=bunuh&id=<?php echo $id; ?>">Bunuh</a> <?php	
 			}
 		}
 		if($_SESSION["karakter"] == "Seer"){
 			if(!isset($_SESSION["terawang"])) {
-				?><a class="btn btn-primary pull-right" href="/ww/play.php?action=terawang&id=<?php echo $id; ?>">Terawang</a> <?php
+				?><a class="btn btn-primary pull-right" href="/play.php?action=terawang&id=<?php echo $id; ?>">Terawang</a> <?php
 			}	
 		}
 		if($_SESSION["karakter"] == "Guard"){
 			if(!isset($_SESSION["lindungi"])) {
-				?><a class="btn btn-primary pull-right" href="/ww/play.php?action=lindungi&id=<?php echo $id; ?>">Lindungi</a> <?php
+				?><a class="btn btn-primary pull-right" href="/play.php?action=lindungi&id=<?php echo $id; ?>">Lindungi</a> <?php
 			}	
 		}
 	}
@@ -166,7 +166,7 @@ $groom = mysqli_fetch_array(mysqli_query($connection, "select * from room where 
 function tampil(){
 	$nama = $_SESSION["masuk"];
 	$karakter = $_SESSION["karakter"];
-	$connection = mysqli_connect("localhost", "root", "");
+	$connection = mysqli_connect("173.208.189.34", "adityaw1_wp197", "Putuaditya97");
 	$db = mysqli_select_db($connection, "adityaw1_wp197");
 	$id = $_GET["id"];
 	$gpemain = mysqli_fetch_array(mysqli_query($connection, "select * from pemain where nama='$nama'"));
@@ -278,7 +278,7 @@ function tampil(){
 	  	<?php echo Main::message(); menang(); ?> 
 		<?php echo cek();  refresh(); ?>
 		<?php echo login(); ?> 
-	  	<form role="form" class="live_form form" id="login_form" method="post" action="/ww/play.php?id=<?php echo $groom["id"]; ?>">
+	  	<form role="form" class="live_form form" id="login_form" method="post" action="/play.php?id=<?php echo $groom["id"]; ?>">
 			<?php echo tampil(); ?>
 	  	</form>
 	</div>
